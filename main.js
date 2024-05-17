2237b819126c5f40ab2f7d5518858233c3f4dba5
Deivison <senhordeivison@gmail.com>
2024-05-17 14:47:40 -0300

    Nivel do Herói

---
+++ main.js
@@ -0,0 +1,30 @@
+let heroi = 'Rex'    // Nome do Herói
+let xp = 5000        // XP do Herói
+let nivel = ''       // Nivel do Herói
+
+if (xp <= 1000) {
+    nivel = 'Ferro'
+}
+else if (xp >= 1000.1 && xp <= 2000) {
+    nivel = 'Bronze'
+}
+else if (xp >= 2000.1 && xp <= 5000) {
+    nivel = 'Prata'
+}
+else if (xp >= 5000.1 && xp <= 7000) {
+    nivel = 'Ouro'
+}
+else if (xp >= 7000.1 && xp <= 8000) {
+    nivel = 'Platina'
+}
+else if (xp >= 8000.1 && xp <= 9000) {
+    nivel = 'Ascendente'
+}
+else if (xp >= 9000.1 && xp <= 10000) {
+    nivel = 'Imortal'
+}
+else{
+    nivel = 'Radiante'
+}
+
+console.log(`O Herói de nome ${heroi} está no nível: ${nivel}`)
